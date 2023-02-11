08ae5740db13588f146ca564074b265ad37ef35f
EiJey11 <arturs.jarovojs2018@gmail.com>
2023-02-11 22:37:06 +0100

    Added Table, Styles, Sort by Type and asc&desc sorting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,114 @@
-import React from 'react';
+import React , {useState, useEffect} from 'react';
 import './App.css';
 import Table from './components/Table';
 
+
 function App() {
+
+  const [companies, setCompanies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("desc");
+  const [selectedType, setSelectedType] = useState("");
+  const [companiesList, setCompaniesList] = useState([]);
+  
+
+  // Fetching data from API
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://my-json-server.typicode.com/capcito/frontend-ws/companies");
+      const data = await response.json();
+      setCompanies(data);
+    }
+    fetchData();
+  }, []);
+
+
+  //Sort by asc and desc
+  const sortCompanies = (companies, sortOrder, selectedType) => {
+    return companies.sort((a, b) => {
+      if (selectedType) {
+        if (sortOrder === "desc") {
+          return a.type.localeCompare(b.type);
+        } else {
+          return b.type.localeCompare(a.type);
+        }
+      } else {
+        if (sortOrder === "desc") {
+          return a.id - b.id;
+        } else {
+          return b.id - a.id;
+        }
+      }
+    });
+  };
+
+  //Search Filter
+  useEffect(() => {
+    setCompaniesList(
+      sortCompanies(
+        selectedType
+          ? companies.filter(company => company.type === selectedType && company.name.toLowerCase().includes(searchTerm.toLowerCase()))
+          : companies.filter(company => company.name.toLowerCase().includes(searchTerm.toLowerCase())),
+        sortOrder,
+        selectedType
+      )
+    );
+  }, [searchTerm, sortOrder, companies, selectedType]);
+
+
+
+  const handleTypeChange = (e) => {
+    setSelectedType(e.target.value);
+  };
+  
+
+
   return (
-    <div className="App">
-        <Table  />
+    <div className='flex flex-col justify-start h-screen w-full md:min-w-[400px] px-2 pt-16 md:pt-12 pb-12'>
+      <div className='relative'>
+        <input
+          className='flex items-start justify-start bg-gray-200 md:w-[30vw] py-2 px-3 rounded-md'
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        {searchTerm !== "" && (
+          <button
+            className='absolute top-0 right-0 mr-3 mt-1'
+            onClick={() => {
+              setSearchTerm("");
+            }}
+          >
+            &times;
+          </button>
+        )}
+      </div>
+      <div className='flex items-center justify-start w-full'>
+        <button
+          className='text-blue-500 underline underline-offset-2 rounded-md mt-6 text-sm font-semibold' 
+          onClick={() => {
+            setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+          }}
+        >
+          Sort {sortOrder === "desc" ? "Ascending" : "Descending"}
+        </button>
+
+      <select className='mt-6 ml-4 px-2 text-blue-500 underline underline-offset-2 rounded-md text-sm font-semibold' value={selectedType} onChange={handleTypeChange}>
+          <option className='text-blue-500 underline underline-offset-2 rounded-md mt-6 text-sm font-semibold' value="">Sort by Type</option>
+            {Array.from(new Set(companies.map((company) => company.type))).map((type) => (
+              <option key={type} value={type}>
+                {type}
+              </option>
+        ))}
+      </select>
+
+      </div>
+
+      <Table companiesList={companiesList} />
+      {companiesList.length === 0 && searchTerm !== "" && (
+        <div className='text-center text-gray-400 mt-4'>No results found</div>
+      )}
     </div>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
-export default function Table() {
+export default function Table({companiesList}) {
   return (
-    <div>Table</div>
+    <table className='flex flex-col justify-center mt-4'>
+      <thead>
+        <tr className='flex flex-row items-center bg-indigo-800 text-white'>
+          <th className='basis-1/12 text-sm md:text-base py-1'>Id</th>
+          <th className='basis-7/12 text-sm md:text-base py-1'>Name</th>
+          <th className='basis-1/12 text-sm md:text-base py-1'>Type</th>
+          <th className='basis-3/12 text-sm md:text-base text-center'>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {companiesList.map((company) => (
+          <tr className='flex flex-row' key={company.id}>
+            <td className='basis-1/12 text-center bg-gray-200 border-b border-white text-sm md:text-base'>{company.id}</td>
+            <td className='basis-7/12 font-semibold pl-2 text-sm md:text-base'>{company.name}</td>
+            <td className='basis-1/12 text-center font-light text-sm md:text-base'>{company.type}</td>
+            <td className='basis-3/12 text-center text-sm md:text-base'>{new Date(company.created_at).toLocaleDateString()}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
